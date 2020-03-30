44b11501d9c4fe7ff32050dd5979f145cffeabe6
Lana <sitdownrightnow2552@gmail.com>
2020-03-30 21:36:52 +0700

    default template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      app
+    >
+    </v-navigation-drawer>
+
+    <v-app-bar clipped-left app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+      </v-app-bar-nav-icon>
+      <v-toolbar-title> App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@Component
+export default class App extends Vue {
+  drawer: boolean = false;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
