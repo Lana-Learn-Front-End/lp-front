7d7802206086a182900f3b6a3e89478a345e178c
Lana <sitdownrightnow2552@gmail.com>
2020-03-30 20:57:01 +0700

    add vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'max-len': ['warn', 120]
   },
 };
