f8cb3505510e0e4f6c3ce81436c1efd26c9d1f14
Lana <sitdownrightnow2552@gmail.com>
2020-04-01 10:26:13 +0700

    install vee-validate

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
-import './plugins/axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
 import vuetify from './plugins/vuetify';
+import './plugins/axios';
+import './plugins/vee-validate';
 
 Vue.config.productionTip = false;
 
---
+++ src/plugins/vee-validate.ts
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import { extend, ValidationObserver, ValidationProvider } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+
+extend('required', {
+  ...required,
+  message: 'This field is required',
+});
+
+extend('codeFormat', {
+  validate: (value: string) => !!value.match(/^[A-Z]{3,}-[0-9]{3,}$/),
+  message: 'Enter a valid code (Ex: ABC-123)',
+});
+
+Vue.component('validation-provider', ValidationProvider);
+
+Vue.component('validation-observer', ValidationObserver);
