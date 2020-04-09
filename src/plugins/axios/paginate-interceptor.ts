f5559d8a581a9ee6e3294bd1086db3a7691958c5
Lana <sitdownrightnow2552@gmail.com>
2020-04-09 21:05:20 +0700

    fix

--- src/plugins/axios/paginate-interceptor.ts
+++ src/plugins/axios/paginate-interceptor.ts
@@ -1,17 +1,17 @@
 import { AxiosResponse } from 'axios';
 
 export default function paginateResponseInterceptor(res: AxiosResponse): AxiosResponse {
-  const data: any = { res };
+  const { data } = res;
   if (data.content && data.pageable) {
     res.data = {
       isFirst: data.last,
       isLast: data.first,
-      numberOfElements: { data },
+      numberOfElements: data.numberOfElements,
       pageNumber: data.number + 1,
-      pageSize: { data },
-      totalElements: { data },
-      totalPages: { data },
-      content: { data },
+      pageSize: data.pageSize,
+      totalElements: data.totalElements,
+      totalPages: data.totalPages,
+      content: data.content,
     };
   }
   return res;
