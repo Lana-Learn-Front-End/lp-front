4df95c581fc1b3ce4b1980b8ec7b3ef2461285a9
Lana <sitdownrightnow2552@gmail.com>
2020-04-12 19:22:31 +0700

    add semicolon

--- src/plugins/index.d.ts
+++ src/plugins/index.d.ts
@@ -1,21 +1,21 @@
 import { AxiosInstance } from 'axios';
-import { SnackBar } from '@/plugins/snackbar';
+import { SnackBar } from '@/plugins/notify/snackbar';
 import { EventBus } from '@/plugins/event-bus';
-import { NotifyModal } from '@/plugins/notify-modal';
+import { NotifyModal } from '@/plugins/notify/notify-modal';
 
 declare module 'vue/types/vue' {
 
   interface Vue {
     $axios: AxiosInstance;
     $snackbar: SnackBar;
-    $modal: NotifyModal
-    $bus: EventBus
+    $modal: NotifyModal;
+    $bus: EventBus;
   }
 
   interface VueConstructor {
     $axios: AxiosInstance;
     $snackbar: SnackBar;
-    $modal: NotifyModal
-    $bus: EventBus
+    $modal: NotifyModal;
+    $bus: EventBus;
   }
 }
