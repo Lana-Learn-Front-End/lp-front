3db65dc08322debcf70d40ac31d9a47ce8628f11
Lana <sitdownrightnow2552@gmail.com>
2020-04-13 22:36:56 +0700

    add date validate rule

--- src/plugins/vee-validate.ts
+++ src/plugins/vee-validate.ts
@@ -7,6 +7,11 @@ extend('required', {
   message: 'This field is required',
 });
 
+extend('date', {
+  validate: (value: string) => !Number.isNaN(Date.parse(value)),
+  message: 'Not a valid date format. Accept YYYY-MM-DD',
+});
+
 extend('codeFormat', {
   validate: (value: string) => !!value.match(/^[A-Za-z]{3,}-[0-9]{3,}$/),
   message: 'Enter a valid code (Ex: ABC-123)',
